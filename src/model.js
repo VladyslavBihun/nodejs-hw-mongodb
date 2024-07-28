ce57b4f2a25f776a4f5094e7d77a3895ec078f75
Vladyslav Bihun <bihunvv@gmail.com>
2024-07-28 21:28:12 +0300

    step 4 done

---
+++ src/model.js
@@ -0,0 +1,34 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: [/\S+@\S+\.\S+/, 'Please fill a valid email address'],
+      required: false,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
